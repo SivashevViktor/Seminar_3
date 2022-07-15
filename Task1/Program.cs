e27c9364dd99f4a2754481ac5b07046e5ef05791
ViktorSivashev <viktor.sivashev@mail.ru>
2022-07-15 21:55:37 +0300

    seminar3

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите X:");
+int X = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите Y:");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if (X == 0 || Y == 0) Console.WriteLine("X и/или Y = 0. Невозможно определить четверть");
+else
+{
+    if (X > 0 && Y > 0) Console.WriteLine("Точка находиться в первой четверти");
+    if (X < 0 && Y > 0) Console.WriteLine("Точка находиться в второй четверти");
+    if (X < 0 && Y < 0) Console.WriteLine("Точка находиться в третьей четверти");
+    if (X > 0 && Y < 0) Console.WriteLine("Точка находиться в четвертой четверти");
+}
